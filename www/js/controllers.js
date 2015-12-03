89568b2f6b7f9a3890efb48a42cbd25aa7197e5e
Samy Ghribi <samy.ghribi@student.ecp.fr>
2015-12-03 16:42:33 +0100

    Next flash

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -12,11 +12,23 @@ angular.module('starter.controllers', [])
             $scope.timeGPS = moment(position.timestamp).format('MMMM Do YYYY, HH:mm:ss.SSSSSS');
             $scope.diff = moment(moment(dateLocale).diff(moment(position.timestamp))).format('SSS [ms]');
             $scope.updates++;
-            $scope.color = 'white';
+
+
+            var microseconds = parseInt(moment(position.timestamp).format('sSSSSSS'));
+            microsecondsNext = microseconds - microseconds%10000000 + 10000000;
+            var delay = (microsecondsNext-microseconds)/1000;
+
+
+            $scope.nextFlash = moment(position.timestamp).add(delay, 'milliseconds').format('HH:mm:ss.SSSSSS');
+            console.log("Next flash in " + delay + " ms");
             $scope.$apply();
+
             $timeout(function() {
-                $scope.color = 'black';
-            }, 100);
+                $scope.color = 'white';
+                $timeout(function() {
+                    $scope.color = 'black';
+                }, 100);
+            }, delay);
         };
 
         var onError = function () {
